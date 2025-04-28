ee2b9a483005afb62c6bc42f70d49e0eda8ce564
hoonify <hoonify@handong.ac.kr>
2025-04-28 10:25:44 +0900

    3차

--- 6조_김동훈.py
+++ 6조_김동훈.py
@@ -7,25 +7,15 @@ input_file = pd.read_csv('1_adults.csv')
 
 def some_function(input_file):
     df=input_file.copy()
-    df.head()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    obj_cols = ['workclass', 'education', 'marital.status', 'occupation', 'relationship', 'race', 'sex', 'native.country', 'income']
+    for col in obj_cols:
+        df[col] = df[col].replace('?', '') # '?'를 공백으로 대체 해 나중에 결측치 처리
+    df=a.first_step(df) # 결측치 처리 및 중복 제거  
+    df=a.bins2(df,'education.num',[0,0.25,0.5,0.75,1],['low','mid','high','very high'],'edu_level') # education.num을 bins로 나누기
+    remove_cols = ['education.num', 'capital.gain', 'capital.loss'] # 제거할 컬럼
+    df=a.remove_cols(df, remove_cols) 
+    num_cols = ['age', 'fnlwgt', 'hours.per.week']
+    df=a.remove_out(df,num_cols)
     return df
 
 output_file = some_function(input_file)
