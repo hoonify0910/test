258f29c168bb7ea40fe3cc70da94333b8f241df5
hoonify <hoonify@handong.ac.kr>
2025-04-28 10:09:29 +0900

    1차 푸시

--- 6조_김동훈.py
+++ 6조_김동훈.py
@@ -3,10 +3,11 @@ import pandas as pd # 데이터프레임 조작
 import numpy as np # 결측치 처리 및 수치 계산
 from sklearn.preprocessing import StandardScaler # Z-score 정규화
 
-input_file = pd.read_csv('some_file.csv')
+input_file = pd.read_csv('1_adults.csv')
 
 def some_function(input_file):
     df=input_file.copy()
+    df.head()
 
 
 
